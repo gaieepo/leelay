d7a18dd761ee15f5aea34066445c25b6ce834791
gaieepo <gaieepo@gmail.com>
2018-11-23 06:19:07 +0800

    revert some refactor and fix namespace problem

--- game.py
+++ game.py
@@ -3,7 +3,35 @@ import numpy as np
 from sgfmill import sgf
 
 from leelaz import leelaz
-from utils import *
+
+SIZE = 19
+EMPTY = 0
+BLACK = 1
+WHITE = 2
+PASS = "pass"
+OPPONENT = {BLACK:WHITE, WHITE:BLACK}
+COL_NAMES = 'ABCDEFGHJKLMNOPQRST'
+
+
+def _coord_to_name(coord):
+    return COL_NAMES[coord[1]] + str(SIZE - coord[0]).upper()
+
+
+def _name_to_coord(name):
+    if name == 'resign':
+        return None
+    return [SIZE - int(name[1:]), COL_NAMES.index(name[0])]
+
+
+def _color_name(color):
+    return {BLACK:'b', WHITE:'w'}[color]
+
+
+def _sgf_move_to_coord(move):
+    # ('b', (15, 16))
+    # black four-three starting point
+    row, col = move[1]
+    return [SIZE - row - 1, col]
 
 
 class _Group:
@@ -17,7 +45,6 @@ class _Group:
     Points are coordinate pairs (row, col).
     """
     pass
-
 
 
 class Game:
